f801b805002ddedc987e71d41a33743078284b19
John J. Hritz <john-j-hritz@sbcglobal.net>
2019-03-28 12:25:52 -0700

    Added Stroustrup's PPP support code as a submodule. Experimented with basic compiletime errors.

--- Chapter 2/2.2 Classic First Program/classicfirstprogram.cpp
+++ Chapter 2/2.2 Classic First Program/classicfirstprogram.cpp
@@ -1,7 +1,9 @@
-#include "../../std_lib_facilities.h"
+// This program outputs the message “Hello, World!” to the monitor
+
+#include "../../Programming-_Principles_and_Practice_Using_Cpp/std_lib_facilities.h"
 
 int main()
 {
-    std::cout << "Hello, World!\n";
+    std::cout << "Hello, World!\n";     //output "Hello, World!"
     return 0;
 }
